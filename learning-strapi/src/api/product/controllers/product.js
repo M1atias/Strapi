10c0c17dcac687ee56cd3f2879eb8fa57abe8b68
M1atias <maty.f.molina4a@gmail.com>
2021-12-22 17:07:53 -0300

    add: Se customizo el controller de la consulta get de product

--- learning-strapi/src/api/product/controllers/product.js
+++ learning-strapi/src/api/product/controllers/product.js
@@ -6,4 +6,10 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::product.product');
+module.exports = createCoreController('api::product.product', ({strapi}) =>({
+    async find(ctx){
+        
+        const products = await strapi.service('api::product.product').find(ctx.query);
+        return products.results.map((product,i)=>`${i}. ${product.title}`);
+    }
+}));
